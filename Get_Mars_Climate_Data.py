a9f9c9d3ca77b93e01164579d611c53639a08cef
VallesMarinerisExplorer <ahorvath1994@gmail.com>
2023-11-10 16:15:38 -0500

    Add files via upload

---
+++ Get_Mars_Climate_Data.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import warnings
+import requests
+
+year = 2020
+month = 2
+day = 10
+hrs = 1
+mins = 1
+sec = 1
+warnings.filterwarnings("ignore")
+url ="http://www-alternate-mars.lmd.jussieu.fr/mcd_python/cgi-bin/mcdcgi.py?var1=t&var2=p&var3=rho&var4=wind&datekeyhtml=1&ls=10.6&localtime=9.&latitude=all&longitude=all&altitude=10.&zkey=3&spacecraft=none&isfixedlt=off&dust=1&hrkey=1&averaging=off&dpi=80&islog=off&colorm=jet&minval=&maxval=&proj=cyl&palt=&plon=&plat=&trans=&iswind=off&latpoint=&lonpoint="
+r = requests.get(url)
+soup = BeautifulSoup(r.text)
+
+for a in soup.find_all('a', href=True):
+    link = "http://www-alternate-mars.lmd.jussieu.fr/mcd_python" + a['href'][2:]
+print(link)
+response = requests.get(link)
+name = str(year) +"_" + str(month) + "_" + str(day) + str(hrs) + "_" + str(mins) + "_" + str(sec) + ".txt"
+if response.status_code == 200:
+    with open(name + ".txt", "w") as file:
+        file.write(response.text)
+        print("File downloaded successfully.")
+else:
+    print("Failed to download the file.")

e0f176ea298875433d67f12398258e0b1bafcf49
VallesMarinerisExplorer <ahorvath1994@gmail.com>
2024-05-23 23:15:29 -0400

    Update VirtualTelescopeLowRes.py

--- SpaceEnvironment/Astronomy/VirtualTelescopeLowRes.py
+++ SpaceEnvironment/Astronomy/VirtualTelescopeLowRes.py
@@ -1,7 +1,13 @@
-import wget
-import PySimpleGUI as sg
+import wget # < Should replace with requests library probably some day
+import FreeSimpleGUI as sg
 from PIL import Image
-#https://alasky.cds.unistra.fr/hips-image-services/hips2fits#projection=SIN&hips=CDS%2FP%2F2MASS%2Fcolor&ra=266.404995&dec=-28.936174&fov=58.6323
+
+""" This script demonstrates a simple virtual telescope where you input a field of view in degrees and a right ascension and declination and
+it will search the alasky database and return the image of the sky at that location. This could be used in a simulator or as a virtual observatory
+of sorts. Note that there are higher resolution alasky models (but not full sky coverage) if you look around on their website: 
+https://aladin.cds.unistra.fr/hips/list"""
+
+# https://alasky.cds.unistra.fr/hips-image-services/hips2fits#projection=SIN&hips=CDS%2FP%2F2MASS%2Fcolor&ra=266.404995&dec=-28.936174&fov=58.6323
 # fov = 3
 # ra = 5.633083
 # dec = 22.0145
